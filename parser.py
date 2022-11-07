4c7fa0ee3e894268cf29f0e9bd0bfef5f49d5f7e
DianaRyz <dianaryzhenkova1@gmail.com>
2022-11-07 23:11:10 +0400

    Getting data from a page

--- parser.py
+++ parser.py
@@ -1,1 +1,11 @@
-#lab1
+from bs4 import BeautifulSoup
+import requests
+
+def parser():
+    url = 'https://yandex.ru/images/search?text=tiger&from=tabbar'
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    img = soup.find_all('img', class_ = 'serp-item__thumb justifier__thumb')
+
+if __name__ == "__main__":
+    parser()
