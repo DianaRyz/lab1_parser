7195284e1c94f18d0965e7ac4fec0af87c306b5f
DianaRyz <dianaryzhenkova1@gmail.com>
2022-11-17 21:44:43 +0400

    Создание папок, получение ссылок на картинки со страниц сайта

--- parser.py
+++ parser.py
@@ -1,11 +1,32 @@
-from bs4 import BeautifulSoup
+import os
+import time
+import shutil
+import cv2
 import requests
+from bs4 import BeautifulSoup
 
-def parser():
-    url = 'https://yandex.ru/images/search?text=tiger&from=tabbar'
-    html_text = requests.get(url).text
-    soup = BeautifulSoup(html_text, 'lxml')
-    img = soup.find_all('img', class_ = 'serp-item__thumb justifier__thumb')
+def parser(typename):
+    if not os.path.exists("D:/dataset/"):
+        os.mkdir("D:/dataset/")
+    if not os.path.exists("D:/dataset/" + typename):
+        os.mkdir("D:/dataset/" + typename)
+
+    if not os.path.exists("D:/test/"):
+        os.mkdir("D:/test/")
+    if not os.path.exists("D:/test/" + typename):
+        os.mkdir("D:/test/" + typename)
+
+    count = 0
+
+    for page in range(0, 99):
+        url = f"https://yandex.ru/images/search?text=" + typename + f"&p={page}"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, "html.parser")
+
+        src_list = []
+        for link in soup.find_all("img", class_="justifier__thumb"):
+            src_list.append(link.get("src"))
 
 if __name__ == "__main__":
-    parser()
+    parser("tiger")
+    parser("leopard")
