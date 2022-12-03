27890368f2163e418d042b54f094dda6d4e46232
DianaRyz <dianaryzhenkova1@gmail.com>
2022-12-03 20:36:27 +0400

    Finish version

--- copy_random.py
+++ copy_random.py
@@ -1,15 +1,14 @@
 import os
 import csv
+import random
 import shutil
 
-
-def write_copy(item: int, class_name: str, copy_path: str, csv_path: str = "copy_dataset.csv") -> None:
-
+def write_copy(item: int, class_name: str, random_path: str, csv_path: str = "copy_random.csv") -> None:
     """
     Record annotation copy element
         item (int): copy number
         class_name (str): tiger or leopard
-        copy_path (str): path to the copy directory
+        random_path (str): path to the another directory
     """
 
     headings = ["Absolute way", "Relative way", "Class"]
@@ -19,22 +18,22 @@ def write_copy(item: int, class_name: str, copy_path: str, csv_path: str = "copy
         )
         write_in_file.writerow(
             {
-                "Absolute way": copy_path,
-                "Relative way": f"dataset/copy_dataset/{class_name}_{str(item).zfill(4)}.jpg",
+                "Absolute way": random_path,
+                "Relative way": f"dataset/random_dataset/{str(item).zfill(5)}.jpg",
                 "Class": class_name,
             }
         )
 
 
-def copy_to_another(path_to_copy: str, path_to_dataset: str, csv_path: str = "copy_dataset.csv") -> str or None:
+def copy_to_random(path_to_dataset: str, path_to_random: str, csv_path: str = "copy_random.csv") -> str or None:
 
     """
-    Copies data
-        path_to_copy(str): path to the copy directory
-        path_to_dataset(str): path to the dataset
+    Copies data with random name
+        dataset_path(str): path to the dataset
+        random_path(str): path to the copy directory
     """
-    if not os.path.exists(path_to_copy):
-        os.mkdir(path_to_copy)
+    if not os.path.exists(path_to_random):
+        os.mkdir(path_to_random)
 
     headings = ["Absolute way", "Relative way", "Class"]
     with open(csv_path, "w", newline="", encoding="utf-8") as f:
@@ -42,6 +41,11 @@ def copy_to_another(path_to_copy: str, path_to_dataset: str, csv_path: str = "co
             f, fieldnames=headings, delimiter=";", quoting=csv.QUOTE_ALL
         )
         writer.writeheader()
+
+    random_numbers = list(range(0, 10001))
+    random.shuffle(random_numbers)
+    random_num1 = random_numbers[: len(random_numbers) // 2]
+    random_num2 = random_numbers[len(random_numbers) // 2:]
 
     class_name = "tiger"
     path_class = path_to_dataset + "/" + class_name
@@ -55,10 +59,10 @@ def copy_to_another(path_to_copy: str, path_to_dataset: str, csv_path: str = "co
 
     for i in range(0, count_files):
         path = path_class + f"/{str(i).zfill(4)}.jpg"
-        new_path = path_class + f"/{class_name}_{str(i).zfill(4)}.jpg"
+        new_path = path_to_random + f"/{str(random_num1[i]).zfill(5)}.jpg"
         if os.path.isfile(path):
             shutil.copyfile(path, new_path)
-            write_copy(i, class_name, new_path, csv_path)
+            write_copy(random_num1[i], class_name, new_path, csv_path)
 
     class_name = "leopard"
     path_class = path_to_dataset + "/" + class_name
@@ -72,13 +76,12 @@ def copy_to_another(path_to_copy: str, path_to_dataset: str, csv_path: str = "co
 
     for i in range(0, count_files):
         path = path_class + f"/{str(i).zfill(4)}.jpg"
-        new_path = path_to_copy + f"/{class_name}_{str(i).zfill(4)}.jpg"
+        new_path = path_to_random + f"/{str(random_num2[i]).zfill(5)}.jpg"
         if os.path.isfile(path):
             shutil.copyfile(path, new_path)
-            write_copy(i, class_name, new_path, csv_path)
-
+            write_copy(random_num2[i], class_name, new_path, csv_path)
 
 if __name__ == "__main__":
-    path_to_dataset = "D:/dataset"
-    path_to_another_dataset = "D:/dataset/copy_dataset"
-    copy_to_another(path_to_another_dataset, path_to_dataset)
+    path_dataset = "D:/dataset"
+    path_random = "D:/dataset/random_dataset"
+    copy_to_random(path_dataset, path_random)
